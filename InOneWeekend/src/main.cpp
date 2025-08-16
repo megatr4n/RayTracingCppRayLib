8d16231e19b2101c01739b552bebbb91674906fe
megatr4n <dahiildahiil07@gmail.com>
2025-08-16 14:46:24 +0300

    Add ray_color function for recursive ray tracing

---
+++ InOneWeekend/src/main.cpp
@@ -0,0 +1,29 @@
+#include "raylib.h"
+#include "vec3.h"
+#include "color.h"
+#include "ray.h"
+#include "hittable.h"
+#include "sphere.h"
+#include "hittable_list.h"
+#include "camera.h"
+#include "material.h"
+
+
+vec3 ray_color(const ray& r, const hittable& world, int depth) {
+    hit_record rec;
+    if (depth <=0) 
+        return vec3(0, 0, 0);
+
+    if (world.hit(r, 0.001, infinity, rec)) {
+        ray scattered;
+        vec3 attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation * ray_color(scattered, world, depth - 1);
+        }
+        return vec3(0, 0, 0);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
